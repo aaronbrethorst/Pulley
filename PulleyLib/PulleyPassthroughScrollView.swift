97a73bc2606a1b7aa396e7b375ad37a131e4462c
Aaron Brethorst <aaron@brethorsting.com>
2018-07-25 13:30:33 -0700

    Fixes more Swiftlint warnings

--- PulleyLib/PulleyPassthroughScrollView.swift
+++ PulleyLib/PulleyPassthroughScrollView.swift
@@ -8,27 +8,27 @@
 
 import UIKit
 
-// swiftlint:disable trailing_whitespace
+// swiftlint:disable line_length
 
 protocol PulleyPassthroughScrollViewDelegate: class {
-    
+
     func shouldTouchPassthroughScrollView(scrollView: PulleyPassthroughScrollView, point: CGPoint) -> Bool
     func viewToReceiveTouch(scrollView: PulleyPassthroughScrollView, point: CGPoint) -> UIView
 }
 
 class PulleyPassthroughScrollView: UIScrollView {
-    
+
     weak var touchDelegate: PulleyPassthroughScrollViewDelegate?
-    
+
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        
+
         if
             let touchDelegate = touchDelegate,
             touchDelegate.shouldTouchPassthroughScrollView(scrollView: self, point: point)
         {
             return touchDelegate.viewToReceiveTouch(scrollView: self, point: point).hitTest(touchDelegate.viewToReceiveTouch(scrollView: self, point: point).convert(point, from: self), with: event)
         }
-        
+
         return super.hitTest(point, with: event)
     }
 }
--- PulleyLib/UIView+constrainToParent.swift
+++ PulleyLib/UIView+constrainToParent.swift
@@ -7,20 +7,21 @@
 
 import UIKit
 
-// swiftlint:disable trailing_whitespace colon
-
 extension UIView {
-    
+
     func constrainToParent() {
         constrainToParent(insets: .zero)
     }
-    
+
     func constrainToParent(insets: UIEdgeInsets) {
         guard let parent = superview else { return }
-        
+
         translatesAutoresizingMaskIntoConstraints = false
-        let metrics: [String : Any] = ["left" : insets.left, "right" : insets.right, "top" : insets.top, "bottom" : insets.bottom]
-        
+        let metrics: [String: Any] = ["left": insets.left,
+                                       "right": insets.right,
+                                       "top": insets.top,
+                                       "bottom": insets.bottom]
+
         parent.addConstraints(["H:|-(left)-[view]-(right)-|", "V:|-(top)-[view]-(bottom)-|"].flatMap {
             NSLayoutConstraint.constraints(withVisualFormat: $0, metrics: metrics, views: ["view": self])
         })
